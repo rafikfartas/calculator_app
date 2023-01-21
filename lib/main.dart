07c0970ff1ec078d3b9fe5daab9197a34df6c695
rafikfartas <rafikawm8@gmail.com>
2023-01-21 12:15:17 +0100

    initinal commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:calculator_app/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter calculator app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(title: 'Flutter calculator app'),
+    );
+  }
+}
